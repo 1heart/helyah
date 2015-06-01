9c20fd13916ed08691c0c636b97d7b1d6adb19e0
1heart <yixin1996@gmail.com>
2015-05-31 20:22:01 -0700

    random choice of html files

--- hello.py
+++ hello.py
@@ -1,8 +1,12 @@
 import os
-from flask import Flask
+import random
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+choices = ['1.html', '2.html']
+
 @app.route('/')
 def hello():
-        return 'Hello World!'
+        choice = random.choice(choices)
+        return render_template(choice) 
